4a03995d812b1e8f8cca9f9d658e12a9be845c12
zhangxianjun <1581758003@qq.com>
2021-08-17 15:41:59 +0800

    feat - js

--- src/main/java/team/flint/flint/controller/WorkbenchController.java
+++ src/main/java/team/flint/flint/controller/WorkbenchController.java
@@ -1,5 +1,7 @@
 package team.flint.flint.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,7 +12,9 @@ import org.springframework.web.servlet.ModelAndView;
 import team.flint.flint.core.service.ProgramService;
 import team.flint.flint.model.table.Resource;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class WorkbenchController {
@@ -19,27 +23,48 @@ public class WorkbenchController {
     private ProgramService programService;
 
     @RequestMapping(value = "/page/workbench")
-    public ModelAndView getWorkbenchPage(ModelAndView modelAndView) {
+    public ModelAndView getWorkbenchPage(ModelAndView modelAndView) throws JsonProcessingException {
+
 //        Date date = new Date();
 //        Marker marker = MarkerFactory.getMarker("Flint");
 //
 ////        logger.error(marker, "{\"label0\":\"xxxx\"}");
         // 获取一级导航
         List<Resource> topList = programService.getResourceList(0);
+        Integer topIndex = topList.get(0).getResourceId();
         modelAndView.addObject("topList", topList);
+        modelAndView.addObject("topIndex", topIndex);
+
         // 获取二级导航
-        List<Resource> leftList = programService.getResourceList(1);
-        modelAndView.addObject("leftList", leftList);
+        Map<String, List<Resource>> leftMap = new HashMap<>();
+        for (int i = 0; i < topList.size(); i++) {
+            Resource r = topList.get(i);
+            List<Resource> leftList = programService.getResourceList(r.getResourceId());
+            leftMap.put(r.getResourceId() + "", leftList);
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        modelAndView.addObject("leftMap", objectMapper.writeValueAsString(leftMap));
         // 获取首页
-        modelAndView.addObject("firstPage", leftList.get(0));
+        List<Resource> leftList = leftMap.get(topIndex+"");
+        modelAndView.addObject("leftList", leftList);
+
+        Resource firstPageModel = leftList.size() > 0 ? leftList.get(0) : null;
+        modelAndView.addObject("firstPageModel", firstPageModel);
 
         modelAndView.setViewName("workbench");
+
         return modelAndView;
     }
 
-    @RequestMapping(value = "/api/test")
+    @RequestMapping(value = "/api/left/menu")
     @ResponseBody
-    public String apiTest() {
-        return "xxx";
+    public String getLeftMenu(Integer resourceId) throws JsonProcessingException {
+
+        List<Resource> leftList = programService.getResourceList(resourceId);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.writeValueAsString(leftList);
     }
 }
