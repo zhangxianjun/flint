f3ba677ddf856fc9a16678486c1c4e92bd895813
zhangxianjun <1581758003@qq.com>
2021-08-26 21:07:03 +0800

    feat - xx

--- src/main/java/team/flint/flint/controller/program/PlanController.java
+++ src/main/java/team/flint/flint/controller/program/PlanController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import team.flint.flint.core.service.ProgramService;
-import team.flint.flint.model.table.Program;
+import team.flint.flint.core.service.SystemService;
+import team.flint.flint.model.table.Resource;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,6 +27,9 @@ import java.util.Map;
 public class PlanController {
 
     @Autowired
+    private SystemService systemService;
+
+    @Autowired
     private ProgramService programService;
 
     @RequestMapping(value = "/page/program/plan/list")
@@ -35,11 +39,11 @@ public class PlanController {
         return modelAndView;
     }
 
-    @RequestMapping(value = "/api/program/plan/list")
+    @RequestMapping(value = "/api/system/navigation/list")
     @ResponseBody
-    public String getProgramListData() throws JsonProcessingException {
+    public String getNavigationListData(Integer resourceId) throws JsonProcessingException {
 
-        List<Program> list = programService.getProgramList();
+        List<Resource> list = systemService.getResourceList(resourceId);
 
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -51,37 +55,48 @@ public class PlanController {
         return objectMapper.writeValueAsString(map);
     }
 
-    @RequestMapping(value = "/page/program/plan/add")
+    @RequestMapping(value = "/page/system/navigation/add")
     @ResponseBody
-    public ModelAndView getAddProgramPage(ModelAndView modelAndView, Integer programId) {
+    public ModelAndView getAddProgramPage(ModelAndView modelAndView, Integer resourceId) {
+        modelAndView.addObject("resourceId", resourceId);
+        // 父级ID
+        List<Resource> rootList = programService.getResourceListByRootId(1);
+        rootList.add(systemService.getResourceById(1));
+        modelAndView.addObject("rootList", rootList);
 
-        if (programId == 0) {
+        Resource resource = new Resource();
 
+        if (resourceId > 0) {
+            resource = systemService.getResourceById(resourceId);
         }
-        modelAndView.setViewName("/program/program_add");
+
+        modelAndView.addObject("resource", resource);
+        modelAndView.setViewName("/system/navigation_add");
 
         return modelAndView;
     }
 
-    @RequestMapping(value = "/api/program/plan/add")
+    @RequestMapping(value = "/api/system/navigation/add")
     @ResponseBody
-    public String addProgramData(String name,
-                                 String code,
-                                 String desc,
-                                 String prototype,
-                                 String source,
-                                 String dev_output,
-                                 String rc_output,
-                                 String pro_output) throws JsonProcessingException {
+    public String addProgramData(Integer resourceId,
+                                 Integer rootId,
+                                 String name,
+                                 String path,
+                                 Integer showOrder) throws JsonProcessingException {
 
+
+        if (resourceId == 0) {
+            // 新增
+        } else {
+            // 修改
+            systemService.editResource(rootId, name, path, showOrder, resourceId);
+        }
 
 
         ObjectMapper objectMapper = new ObjectMapper();
-
         Map<String, Object> map = new HashMap<>();
         map.put("code", 0);
         map.put("msg", "成功");
-//        map.put("data", list);
 
         return objectMapper.writeValueAsString(map);
     }
